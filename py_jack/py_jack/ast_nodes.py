b9aa55daf4f41115e53b647bab13fdbb931e7281
sasa cocic <cocicsasa@gmail.com>
2024-08-30 12:11:15 -0400

    add context manager for writing tags + change tabs to spaces for indentation

--- py_jack/py_jack/ast_nodes.py
+++ py_jack/py_jack/ast_nodes.py
@@ -3,30 +3,35 @@ import py_jack.scanner as scanner
 import pathlib
 import typing
 import dataclasses
+import contextlib
 
 
 class XmlWriter(typing.Protocol):
     def write_xml(self, file: typing.IO, indent: int = 0): ...
     def write_token(self, token: scanner.Token, file: typing.IO, indent: int = 0):
         if token.token_type in scanner.token_variants["keywords"]:
-            file.write(f"{'\t' * indent}<keyword> {token.lexeme} </keyword>\n")
+            file.write(f"{' ' * indent}<keyword> {token.lexeme} </keyword>\n")
         elif token.token_type in scanner.token_variants["symbols"]:
-            file.write(f"{'\t' * indent}<symbol> {token.lexeme} </symbol>\n")
+            file.write(f"{' ' * indent}<symbol> {token.lexeme} </symbol>\n")
         else:
             file.write(
-                f"{'\t' * indent}<{token.token_type.name.lower()}> {token.lexeme} </{token.token_type.name.lower()}>\n"
+                f"{' ' * indent}<{token.token_type.name.lower()}> {token.lexeme} </{token.token_type.name.lower()}>\n"
             )
 
     def write_string(
         self, surround: str, element: str, file: typing.IO, indent: int = 0
     ):
-        file.write(f"{'\t' * indent}<{surround}> {element} </{surround}>\n")
+        file.write(f"{' ' * indent}<{surround}> {element} </{surround}>\n")
 
-    def write_node(self, file, indent: int, is_opening_tag: bool = True):
-        if is_opening_tag:
-            file.write(f"{'\t' * indent}<{type(self).__name__.lower()}>\n")
-        else:
-            file.write(f"{'\t' * indent}</{type(self).__name__.lower()}>\n")
+    @contextlib.contextmanager
+    def write_node(self, file, indent: int):
+        file.write(f"{' ' * indent}<{self.camel_case()}>\n")
+        yield
+        file.write(f"{' ' * indent}</{self.camel_case()}>\n")
+
+    def camel_case(self):
+        name = type(self).__name__
+        return name[0].lower() + name[1:]
 
 
 @dataclasses.dataclass
@@ -48,6 +53,7 @@ class ClassNode(XmlWriter):
         #     mode = "w"
         with file_path.open(mode) as xml_file:
             xml_file.write(f"<{self.class_kw.lexeme}>\n")
+            indent += 1
             self.write_token(self.class_kw, xml_file, indent=indent)
             self.write_token(self.class_name, xml_file, indent=indent)
             self.write_string(
@@ -73,18 +79,21 @@ class ClassVarDec(XmlWriter):
     var_names: list[tuple[scanner.Token, scanner.Token]] | None = None
 
     def write_xml(self, file: typing.IO, indent: int):
-        file.write(f"{'\t' * indent}<classVarDec>\n")
-        indent += 1
-        self.write_token(self.field_type, file, indent=indent)
-        self.write_token(self.type_, file, indent=indent)
-        self.write_token(self.var_name, file, indent=indent)
+        # file.write(f"{' ' * indent}<classVarDec>\n")
+        with self.write_node(file, indent=indent):
+            indent += 2
+            self.write_token(self.field_type, file, indent=indent)
+            self.write_token(self.type_, file, indent=indent)
+            self.write_token(self.var_name, file, indent=indent)
 
-        for comma, var_name in self.var_names or []:
-            self.write_token(var_name, file, indent=indent)
-            self.write_token(comma, file, indent=indent)
+            for comma, var_name in self.var_names or []:
+                self.write_token(var_name, file, indent=indent)
+                self.write_token(comma, file, indent=indent)
 
-        self.write_string("symbol", self.semi_colon.lexeme, file=file, indent=indent)
-        file.write(f"{'\t' * (indent - 1)}</classVarDec>\n")
+            self.write_string(
+                "symbol", self.semi_colon.lexeme, file=file, indent=indent
+            )
+        # file.write(f"{' ' * indent}</classVarDec>\n")
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}( {self.field_type}  | {self.field_type} | {self.var_name}) "
@@ -101,16 +110,16 @@ class SubroutineDec(XmlWriter):
     parameter_list: ParameterList
 
     def write_xml(self, file: typing.IO, indent: int):
-        self.write_node(file, indent=indent)
-        indent += 1
-        self.write_token(self.subroutine_variant, file, indent)
-        self.write_token(self.return_type, file, indent)
-        self.write_token(self.name, file, indent)
-        self.write_token(self.left_paren, file, indent)
-        self.parameter_list.write_xml(file, indent)
-        self.write_token(self.right_paren, file, indent)
-        # TODO: stopped here
-        self.write_node(file, indent=indent, is_opening_tag=False)
+        with self.write_node(file, indent=indent):
+            indent += 2
+            self.write_token(self.subroutine_variant, file, indent)
+            self.write_token(self.return_type, file, indent)
+            self.write_token(self.name, file, indent)
+            self.write_token(self.left_paren, file, indent)
+            self.parameter_list.write_xml(file, indent)
+            self.write_token(self.right_paren, file, indent)
+            self.subroutine_body.write_xml(file, indent)
+            # self.write_node(file, indent=indent, is_opening_tag=False)
 
 
 @dataclasses.dataclass
@@ -122,20 +131,18 @@ class ParameterList(XmlWriter):
     )
 
     def write_xml(self, file: typing.IO, indent: int = 0):
-        self.write_node(
-            file, indent
-        )  # is this not a great place for a context manager????
-        if self._type is None or self.varname is None:
-            self.write_node(file, indent, is_opening_tag=False)
-            return
-        indent += 1
-        self.write_token(self._type, file, indent)
-        self.write_token(self.varname, file, indent)
-        for comma, next_type, next_varname in self.parameters:
-            self.write_token(comma, file, indent)
-            self.write_token(next_type, file, indent)
-            self.write_token(next_varname, file, indent)
-        self.write_node(file, indent, is_opening_tag=False)
+        with self.write_node(file, indent) as t:
+            if self._type is None or self.varname is None:
+                # self.write_node(file, indent, is_opening_tag=False)
+                return
+            indent += 2
+            self.write_token(self._type, file, indent)
+            self.write_token(self.varname, file, indent)
+            for comma, next_type, next_varname in self.parameters:
+                self.write_token(comma, file, indent)
+                self.write_token(next_type, file, indent)
+                self.write_token(next_varname, file, indent)
+            # self.write_node(file, indent, is_opening_tag=False)
 
 
 @dataclasses.dataclass
@@ -158,7 +165,9 @@ class SubroutineBody(XmlWriter):
     right_squerly: scanner.Token
 
     def write_xml(self, file: typing.IO, indent: int):
-        raise NotImplementedError("not implemented")
+        # TODO: stopped here
+        with self.write_node(file, indent):
+            pass
 
 
 @dataclasses.dataclass
