28862e3f89dc3b028b912fd996c246703c56e72a
Sasa Cocic-Banjanac <cocicsasa@gmail.com>
2024-09-10 16:33:44 +0400

    implement control flow (#9)
    * implement control flow
    
    * fix improper desugaring of for loop

--- pylox/pylox/Expr.py
+++ pylox/pylox/Expr.py
@@ -1,73 +1,62 @@
 from __future__ import annotations
 from pylox.tokens import Token
 import typing
-
-
 class Expr(typing.Protocol):
-    def accept[T](self, visitor: Visitor[T]) -> T: ...
-
+   def accept[T](self, visitor: Visitor[T]) -> T:...
 
 class Visitor[T]:
-    def visit_AssignExpr(self, expr: Assign) -> T: ...
+   def visit_AssignExpr(self, expr:Assign) -> T:...
 
-    def visit_BinaryExpr(self, expr: Binary) -> T: ...
+   def visit_BinaryExpr(self, expr:Binary) -> T:...
 
-    def visit_GroupingExpr(self, expr: Grouping) -> T: ...
+   def visit_GroupingExpr(self, expr:Grouping) -> T:...
 
-    def visit_LiteralExpr(self, expr: Literal) -> T: ...
+   def visit_LiteralExpr(self, expr:Literal) -> T:...
 
-    def visit_UnaryExpr(self, expr: Unary) -> T: ...
+   def visit_LogicalExpr(self, expr:Logical) -> T:...
 
-    def visit_VariableExpr(self, expr: Variable) -> T: ...
+   def visit_UnaryExpr(self, expr:Unary) -> T:...
 
+   def visit_VariableExpr(self, expr:Variable) -> T:...
 
 class Assign(Expr):
-    def __init__(self, name: Token, value: Expr):
-        self.name = name
-        self.value = value
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_AssignExpr(self)
-
-
+   def __init__(self, name: Token, value: Expr):
+      self.name = name
+      self.value = value
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_AssignExpr(self)
 class Binary(Expr):
-    def __init__(self, left: Expr, operator: Token, right: Expr):
-        self.left = left
-        self.operator = operator
-        self.right = right
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_BinaryExpr(self)
-
-
+   def __init__(self, left: Expr , operator: Token , right: Expr):
+      self.left = left
+      self.operator = operator
+      self.right = right
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_BinaryExpr(self)
 class Grouping(Expr):
-    def __init__(self, expression: Expr):
-        self.expression = expression
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_GroupingExpr(self)
-
-
+   def __init__(self, expression: Expr):
+      self.expression = expression
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_GroupingExpr(self)
 class Literal(Expr):
-    def __init__(self, value: object):
-        self.value = value
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_LiteralExpr(self)
-
-
+   def __init__(self, value: object):
+      self.value = value
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_LiteralExpr(self)
+class Logical(Expr):
+   def __init__(self, left: Expr, operator: Token, right: Expr):
+      self.left = left
+      self.operator = operator
+      self.right = right
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_LogicalExpr(self)
 class Unary(Expr):
-    def __init__(self, operator: Token, right: Expr):
-        self.operator = operator
-        self.right = right
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_UnaryExpr(self)
-
-
+   def __init__(self, operator: Token, right: Expr):
+      self.operator = operator
+      self.right = right
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_UnaryExpr(self)
 class Variable(Expr):
-    def __init__(self, name: Token):
-        self.name = name
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_VariableExpr(self)
+   def __init__(self, name: Token):
+      self.name = name
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_VariableExpr(self)
--- pylox/pylox/Stmnt.py
+++ pylox/pylox/Stmnt.py
@@ -2,50 +2,53 @@ from __future__ import annotations
 from pylox.Expr import Expr
 from pylox.tokens import Token
 import typing
-
-
 class Stmnt(typing.Protocol):
-    def accept[T](self, visitor: Visitor[T]) -> T: ...
-
+   def accept[T](self, visitor: Visitor[T]) -> T:...
 
 class Visitor[T]:
-    def visit_BlockStmnt(self, stmnt: Block) -> T: ...
+   def visit_BlockStmnt(self, stmnt:Block) -> T:...
 
-    def visit_ExpressionStmnt(self, stmnt: Expression) -> T: ...
+   def visit_ExpressionStmnt(self, stmnt:Expression) -> T:...
 
-    def visit_PrintStmnt(self, stmnt: Print) -> T: ...
+   def visit_IfStmnt(self, stmnt:If) -> T:...
 
-    def visit_VarStmnt(self, stmnt: Var) -> T: ...
+   def visit_PrintStmnt(self, stmnt:Print) -> T:...
 
+   def visit_VarStmnt(self, stmnt:Var) -> T:...
+
+   def visit_WhileStmnt(self, stmnt:While) -> T:...
 
 class Block(Stmnt):
-    def __init__(self, statements: list[Stmnt]):
-        self.statements = statements
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_BlockStmnt(self)
-
-
+   def __init__(self, statements: list[Stmnt]):
+      self.statements = statements
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_BlockStmnt(self)
 class Expression(Stmnt):
-    def __init__(self, expression: Expr):
-        self.expression = expression
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_ExpressionStmnt(self)
-
-
+   def __init__(self, expression: Expr):
+      self.expression = expression
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_ExpressionStmnt(self)
+class If(Stmnt):
+   def __init__(self, condition: Expr, then_branch: Stmnt, else_branch: Stmnt | None):
+      self.condition = condition
+      self.then_branch = then_branch
+      self.else_branch = else_branch
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_IfStmnt(self)
 class Print(Stmnt):
-    def __init__(self, expression: Expr):
-        self.expression = expression
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_PrintStmnt(self)
-
-
+   def __init__(self, expression: Expr):
+      self.expression = expression
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_PrintStmnt(self)
 class Var(Stmnt):
-    def __init__(self, name: Token, initializer: Expr):
-        self.name = name
-        self.initializer = initializer
-
-    def accept[T](self, visitor: Visitor[T]):
-        return visitor.visit_VarStmnt(self)
+   def __init__(self, name: Token, initializer: Expr):
+      self.name = name
+      self.initializer = initializer
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_VarStmnt(self)
+class While(Stmnt):
+   def __init__(self, condition: Expr, body: Stmnt):
+      self.condition = condition
+      self.body = body
+   def accept[T](self, visitor: Visitor[T]):
+      return visitor.visit_WhileStmnt(self)
